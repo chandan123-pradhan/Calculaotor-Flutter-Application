770ef479a24e9690bac5ab545f4ef195e50279d8
Chandan <chandanpradhanbxr@gmail.com>
2020-06-06 06:27:28 +0530

    This is a Calculator mobile Application for Android and iOS

---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+
+void main()=>runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Calcutlator App",
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new HomePage(),
+
+      
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  
+  @override
+  State createState() => new HomePageState();
+  
+}
+
+class HomePageState extends State<HomePage> {
+  var num1=0,num2=0,sum=0;
+  final TextEditingController t1=new TextEditingController(text: "0");
+  final TextEditingController t2=new TextEditingController(text: "0");
+  
+  
+  void doAddition(){
+    this.setState(() {
+      num1=int.parse(t1.text);
+      num2=int.parse(t2.text);
+      sum=num1+num2;
+    });
+    
+    }
+
+    void doSub(){
+    this.setState(() {
+      num1=int.parse(t1.text);
+      num2=int.parse(t2.text);
+      sum=num1-num2;
+    });
+    
+    }
+    void doMul(){
+    this.setState(() {
+      num1=int.parse(t1.text);
+      num2=int.parse(t2.text);
+      sum=num1*num2;
+    });
+    
+    }
+    void doDiv(){
+    this.setState(() {
+      num1=int.parse(t1.text);
+      num2=int.parse(t2.text);
+      sum=num1~/num2;//truncating division
+    });
+    
+    }
+
+    void doClear(){
+      this.setState(() {
+        t1.text="0";
+        t2.text="0";
+      });
+    }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Calculator"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(40.0),
+       child: new Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[
+           new Text("Output : $sum",
+           style: new TextStyle
+           (
+            fontSize: 20.0,
+            color: Colors.purple,
+            fontWeight: FontWeight.bold,
+           ),
+           ),
+           new TextField(
+             keyboardType: TextInputType.number,
+             decoration: new InputDecoration(
+             hintText: "Enter First Number:",
+            ),
+            controller: t1,
+           ),//textField
+
+           new TextField(
+             keyboardType: TextInputType.number,
+             decoration: new InputDecoration(
+               hintText: "Enter Second Number:"
+               
+             ),
+             controller: t2,
+           ),//textField
+           new Padding(
+             padding: const EdgeInsets.only(top: 20.0),
+           ),
+            
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("+"),
+                  color: Colors.greenAccent,
+                  onPressed: doAddition,
+                  ),
+                   new MaterialButton(
+                  child: new Text("-"),
+                  color: Colors.greenAccent,
+                  onPressed: doSub,
+                  ),
+                  
+              ],
+              ),
+              new Padding(
+             padding: const EdgeInsets.only(top: 20.0),
+           ),
+            
+             new Row(
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("*"),
+                  
+                  color: Colors.greenAccent,
+                  onPressed: doMul,
+                  ),
+                   new MaterialButton(
+                  child: new Text("/"),
+                  color: Colors.greenAccent,
+                  onPressed: doDiv,
+                  ),
+                  
+              ],
+              ),
+            new Padding(
+             padding: const EdgeInsets.only(top: 20.0),
+           ),
+           new Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+                new MaterialButton(
+                  child: new Text("Clear"),
+                  color: Colors.greenAccent,
+                  onPressed: doClear,
+                  ),
+                  
+           
+        
+           
+             ],
+           )
+        
+           
+         ],
+
+        )
+      ),
+      
+    );
+  }
+}
